c3203a7c75b6cce3a3c974c36c16f6b98b6d3bf7
浪子 <jkjoy@live.cn>
2025-05-04 20:15:39 +0800

    Update archive.php

--- archive.php
+++ archive.php
@@ -2,5 +2,6 @@
 <?php $this->need('head.php'); ?>
 <body class="article-page">
 <?php $this->need('header.php'); ?>
+<?php $this->need('sidebar.php'); ?>
 <?php $this->need('postlist.php'); ?>
 <?php $this->need('footer.php'); ?>
